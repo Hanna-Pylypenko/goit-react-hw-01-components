999b1e7473027fa29d2af1ee2a20bf2597a5effb
Hanna-Pylypenko <pilipenkog679@gmail.com>
2022-07-03 22:03:56 +0300

    updated template file

--- src/index.js
+++ src/index.js
@@ -2,9 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from 'components/App';
 import './index.css';
-
+import { Profile } from 'components/Profile';
+import user from 'user.json';
+const userData = user;
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <App>
+      <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        avatar={userData.avatar}
+        followers={userData.stats.followers}
+        views={userData.stats.views}
+        likes={userData.stats.likes}
+      />
+    </App>
   </React.StrictMode>
 );
+console.log(user);
