03c07109430573ae62b10d30f3084e30478b8aa4
Hanna-Pylypenko <pilipenkog679@gmail.com>
2022-07-05 16:20:51 +0300

    Update TransactionHistoryItems.jsx

--- src/components/TransactionHisoryItems/TransactionHistoryItems.jsx
+++ src/components/TransactionHisoryItems/TransactionHistoryItems.jsx
@@ -2,7 +2,6 @@ import PropTypes from 'prop-types';
 
 import css from "./TransactionHistoryItems.module.css"
 export const TransactionHistoryItem = ({ item }) => {
-    console.log({item})
     return (<tr className="tableRow">
         <td className={css.tableData}>{item.type }</td>
         <td className={css.tableData}>{ item.amount}</td>
